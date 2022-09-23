e15b3dd0cfb55ca69690eba380c41e1427fb4004
Algirdas Urbonavicius <algirdas.urbonavicius@gmail.com>
2022-09-23 11:32:51 +0300

    Added web functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+const arrowDown = document.querySelector(".hero__arrow-down-box");
+const section2 = document.querySelector("#section-2");
+const btnNav = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+const hero = document.querySelector(".hero")
+
+arrowDown.addEventListener("click", function (e) {
+  e.preventDefault();
+  section2.scrollIntoView({ behavior: "smooth" });
+});
+
+btnNav.addEventListener("click", () => {
+  header.classList.toggle("nav-open");
+  hero.classList.toggle("nav-open");
+});
